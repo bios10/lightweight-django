a0c247efa2018bfb8299eaf10a161d1d22962e08
Mauro Dattilo <mau.dattilo@gmail.com>
2015-02-05 15:18:47 +0100

    Chapter 1: Add WSGI application settings to work with Gunicorn

--- chapter_1/hello_world.py
+++ chapter_1/hello_world.py
@@ -2,6 +2,7 @@ import sys
 
 from django.conf import settings
 from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
 
@@ -25,6 +26,8 @@ def index(request):
 urlpatterns = (
     url(r'^$', index),
 )
+
+application = get_wsgi_application()
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
