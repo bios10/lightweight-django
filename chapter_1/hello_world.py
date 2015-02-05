5a93aaac9b99b75f75effb027ce95c2c70249528
Mauro Dattilo <mau.dattilo@gmail.com>
2015-02-05 13:10:30 +0100

    Chapter 1: Add the URL pattern

--- chapter_1/hello_world.py
+++ chapter_1/hello_world.py
@@ -1,5 +1,11 @@
+from django.conf.urls import url
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse('Hello World!')
+
+
+urlpatterns = (
+    url(r'^$', index),
+)
