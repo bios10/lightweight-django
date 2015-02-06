fea7e18193ab8a6a409d6511f5124501235f056c
Mauro Dattilo <mau.dattilo@gmail.com>
2015-02-06 14:44:43 +0100

    Chapter 3: Add get_page_or_404 and page views

--- chapter_3/prototypes.py
+++ chapter_3/prototypes.py
@@ -1,5 +1,10 @@
+import os
 import sys
+
 from django.conf import settings
+
+
+BASE_DIR = os.path.dirname(__file__)
 
 settings.configure(
     DEBUG=True,
@@ -12,7 +17,9 @@ settings.configure(
         'sitebuilder',
     ),
     STATIC_URL='/static/',
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages')
 )
+
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
