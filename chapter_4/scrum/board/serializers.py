866f03c7d2cebff8f7a2bac98cfde47915867cc0
Mauro Dattilo <mau.dattilo@gmail.com>
2015-02-07 23:30:08 +0100

    Chapter 4: Add User serializer

--- chapter_4/scrum/board/serializers.py
+++ chapter_4/scrum/board/serializers.py
@@ -25,3 +25,11 @@ class TaskSerializer(serializers.ModelSerializer):
 
     def get_status_display(self, obj):
         return obj.get_status_display()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', )
