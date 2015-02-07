2aaf1b544fa2467b6828139e73e063d8fce8fba3
Mauro Dattilo <mau.dattilo@gmail.com>
2015-02-08 00:55:12 +0100

    Chapter 4: Add various filters to view sets

--- chapter_4/scrum/board/serializers.py
+++ chapter_4/scrum/board/serializers.py
@@ -20,6 +20,7 @@ class SprintSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return {
             'self': reverse('sprint-detail', kwargs={'pk': obj.pk}, request=request),
+            'tasks': reverse('task-list', request=request) + '?sprint={}'.format(obj.pk),
         }
 
 
@@ -65,4 +66,5 @@ class UserSerializer(serializers.ModelSerializer):
         username = obj.get_username()
         return {
             'self': reverse('user-detail', kwargs={User.USERNAME_FIELD: username}, request=request),
+            'tasks': '{}?assigned={}'.format(reverse('task-list', request=request), username)
         }
